c638c8e7528735f134f2ea13be68bce5adb73e76
DuncanRowland <duncan.a.rowland@gmail.com>
2015-10-22 19:42:10 +0100

    Sample scripts

--- scripts.py
+++ scripts.py
@@ -1,20 +1,145 @@
 scripts = { \
-'BIP0': \
+'OPEN': \
 [ \
-  {"id":"0", "target":30, "pause":0.0, "delay":5.0}, \
-  {"id":"0", "target":40, "pause":5.1}, \
-  {"id":"0", "target":30, "pause":5.2}, \
-  {"id":"0", "target":40, "pause":5.3}, \
-  {"id":"0", "target":30, "pause":5.4}  \
+  {"id":"0", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"1", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"2", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"3", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"4", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"5", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"6", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"7", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"8", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"9", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"10", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"11", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"12", "target":100, "pause":0.0, "delay":0.0}, \
+  {"id":"13", "target":100, "pause":0.0, "delay":0.0} \
 ], \
-'BIP1': \
+'CLOSE': \
 [ \
-  {"id":"1", "target":30, "pause":0.0, "delay":5.0}, \
-  {"id":"1", "target":40, "pause":5.1}, \
-  {"id":"1", "target":30, "pause":5.2}, \
-  {"id":"1", "target":40, "pause":5.3}, \
-  {"id":"1", "target":30, "pause":5.4}  \
+  {"id":"0", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"1", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"2", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"3", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"4", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"5", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"6", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"7", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"8", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"9", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"10", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"11", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"12", "target":0, "pause":0.0, "delay":0.0}, \
+  {"id":"13", "target":0, "pause":0.0, "delay":0.0} \
+], \
+'SHAKEN': \
+[ \
+  {"id":"0", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"1", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"2", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"3", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"4", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"5", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"6", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"7", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"8", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"9", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"10", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"11", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"12", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"13", "target":50, "pause":0.0, "delay":0.0}, \
+  \
+  {"id":"0", "target":60, "pause":10.0, "delay":0.0}, \
+  {"id":"1", "target":60, "pause":10.1, "delay":0.0}, \
+  {"id":"2", "target":60, "pause":10.2, "delay":0.0}, \
+  {"id":"3", "target":60, "pause":10.3, "delay":0.0}, \
+  {"id":"4", "target":60, "pause":10.4, "delay":0.0}, \
+  {"id":"5", "target":60, "pause":10.5, "delay":0.0}, \
+  {"id":"6", "target":60, "pause":10.6, "delay":0.0}, \
+  {"id":"7", "target":60, "pause":10.7, "delay":0.0}, \
+  {"id":"8", "target":60, "pause":10.8, "delay":0.0}, \
+  {"id":"9", "target":60, "pause":10.9, "delay":0.0}, \
+  {"id":"10", "target":60, "pause":11.0, "delay":0.0}, \
+  {"id":"11", "target":60, "pause":11.1, "delay":0.0}, \
+  {"id":"12", "target":60, "pause":11.2, "delay":0.0}, \
+  {"id":"13", "target":60, "pause":11.3, "delay":0.0}, \
+  \
+  {"id":"0", "target":50, "pause":10.1, "delay":0.0}, \
+  {"id":"1", "target":50, "pause":10.2, "delay":0.0}, \
+  {"id":"2", "target":50, "pause":10.3, "delay":0.0}, \
+  {"id":"3", "target":50, "pause":10.4, "delay":0.0}, \
+  {"id":"4", "target":50, "pause":10.5, "delay":0.0}, \
+  {"id":"5", "target":50, "pause":10.6, "delay":0.0}, \
+  {"id":"6", "target":50, "pause":10.7, "delay":0.0}, \
+  {"id":"7", "target":50, "pause":10.8, "delay":0.0}, \
+  {"id":"8", "target":50, "pause":10.9, "delay":0.0}, \
+  {"id":"9", "target":50, "pause":11.0, "delay":0.0}, \
+  {"id":"10", "target":50, "pause":11.1, "delay":0.0}, \
+  {"id":"11", "target":50, "pause":11.2, "delay":0.0}, \
+  {"id":"12", "target":50, "pause":11.3, "delay":0.0}, \
+  {"id":"13", "target":50, "pause":11.4, "delay":0.0}, \
+  \
+  {"id":"0", "target":60, "pause":10.2, "delay":0.0}, \
+  {"id":"1", "target":60, "pause":10.3, "delay":0.0}, \
+  {"id":"2", "target":60, "pause":10.4, "delay":0.0}, \
+  {"id":"3", "target":60, "pause":10.5, "delay":0.0}, \
+  {"id":"4", "target":60, "pause":10.6, "delay":0.0}, \
+  {"id":"5", "target":60, "pause":10.7, "delay":0.0}, \
+  {"id":"6", "target":60, "pause":10.8, "delay":0.0}, \
+  {"id":"7", "target":60, "pause":10.9, "delay":0.0}, \
+  {"id":"8", "target":60, "pause":11.0, "delay":0.0}, \
+  {"id":"9", "target":60, "pause":11.1, "delay":0.0}, \
+  {"id":"10", "target":60, "pause":11.2, "delay":0.0}, \
+  {"id":"11", "target":60, "pause":11.3, "delay":0.0}, \
+  {"id":"12", "target":60, "pause":11.4, "delay":0.0}, \
+  {"id":"13", "target":60, "pause":11.5, "delay":0.0} \
+], \
+'STIRRED': \
+[ \
+  {"id":"0", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"1", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"2", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"3", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"4", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"5", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"6", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"7", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"8", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"9", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"10", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"11", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"12", "target":50, "pause":0.0, "delay":0.0}, \
+  {"id":"13", "target":50, "pause":0.0, "delay":0.0}, \
+  \
+  {"id":"0", "target":100, "pause":10.0, "delay":0.0}, \
+  {"id":"1", "target":100, "pause":10.1, "delay":0.0}, \
+  {"id":"2", "target":100, "pause":10.2, "delay":0.0}, \
+  {"id":"3", "target":100, "pause":10.3, "delay":0.0}, \
+  {"id":"4", "target":100, "pause":10.4, "delay":0.0}, \
+  {"id":"5", "target":100, "pause":10.5, "delay":0.0}, \
+  {"id":"6", "target":100, "pause":10.6, "delay":0.0}, \
+  {"id":"7", "target":100, "pause":10.7, "delay":0.0}, \
+  {"id":"8", "target":100, "pause":10.8, "delay":0.0}, \
+  {"id":"9", "target":100, "pause":10.9, "delay":0.0}, \
+  {"id":"10", "target":100, "pause":11.0, "delay":0.0}, \
+  {"id":"11", "target":100, "pause":11.1, "delay":0.0}, \
+  {"id":"12", "target":100, "pause":11.2, "delay":0.0}, \
+  {"id":"13", "target":100, "pause":11.3, "delay":0.0}, \
+  \
+  {"id":"0", "target":0, "pause":15.0, "delay":0.0}, \
+  {"id":"1", "target":0, "pause":15.1, "delay":0.0}, \
+  {"id":"2", "target":0, "pause":15.2, "delay":0.0}, \
+  {"id":"3", "target":0, "pause":15.3, "delay":0.0}, \
+  {"id":"4", "target":0, "pause":15.4, "delay":0.0}, \
+  {"id":"5", "target":0, "pause":15.5, "delay":0.0}, \
+  {"id":"6", "target":0, "pause":15.6, "delay":0.0}, \
+  {"id":"7", "target":0, "pause":15.7, "delay":0.0}, \
+  {"id":"8", "target":0, "pause":15.8, "delay":0.0}, \
+  {"id":"9", "target":0, "pause":15.9, "delay":0.0}, \
+  {"id":"10", "target":0, "pause":16.0, "delay":0.0}, \
+  {"id":"11", "target":0, "pause":16.1, "delay":0.0}, \
+  {"id":"12", "target":0, "pause":16.2, "delay":0.0}, \
+  {"id":"13", "target":0, "pause":16.3, "delay":0.0} \
 ] \
 }
-
-
